87c3c9ae6779d8ddd427ed7d364b61024b1ca2b8
Lukas <lpetersonrhs@gmail.com>
2021-11-16 19:26:07 -0600

    Adding animate styles

--- src/pages/DevicesPage/DevicesMobileView.tsx
+++ src/pages/DevicesPage/DevicesMobileView.tsx
@@ -5,12 +5,26 @@ import DeviceList from "./DevicesList";
 import DeviceDetails from "./DeviceDetails";
 import { isLoading } from "../../core/stores/utils";
 import { devicePath } from "./index";
+import {
+  AnimatedRoutes,
+  RouteTransition,
+} from "../../components/responsive/animation/route-transition";
+
+//Shamelessly stolen from https://gist.github.com/stolinski/f33bbd5e01f37dd9e9003f0f40f55a4f#file-slidein-tsx-L26
 
 const MobileRoutes: VFC = () => {
   return (
     <>
-      <Route path="/devices" exact component={DeviceList} />
-      <Route path={devicePath} component={DeviceDetails} />
+      {/* <Route path="/devices" exact component={DeviceList} />
+      <Route path={devicePath} component={DeviceDetails} /> */}
+      <AnimatedRoutes exitBeforeEnter initial={false}>
+        <RouteTransition exact path="/devices">
+          <DeviceList />
+        </RouteTransition>
+        <RouteTransition path={devicePath}>
+          <DeviceDetails />
+        </RouteTransition>
+      </AnimatedRoutes>
     </>
   );
 };
