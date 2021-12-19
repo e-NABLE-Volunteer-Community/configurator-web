20efa198e6891b4a9d4d2c93be9e20a8cb3cb4f5
Lukas <lpetersonrhs@gmail.com>
2021-12-19 10:26:47 -0600

    Small bugfix for div ID mockup

--- src/core/stores/profiles.ts
+++ src/core/stores/profiles.ts
@@ -9,27 +9,27 @@ const mockProfiles = (): Profile[] => [
   },
   {
     name: "Bigsby Smith",
-    profileId: "567",
+    profileId: "5627",
     profileImageUrl: "https://i.pravatar.cc/120?img=2",
   },
   {
     name: "Bill Bless",
-    profileId: "123",
+    profileId: "1233",
     profileImageUrl: "https://i.pravatar.cc/120?img=3",
   },
   {
     name: "Hudson Lee",
-    profileId: "567",
+    profileId: "5647",
     profileImageUrl: "https://i.pravatar.cc/120?img=4",
   },
   {
     name: "Alix Perez",
-    profileId: "123",
+    profileId: "1253",
     profileImageUrl: "https://i.pravatar.cc/120?img=5",
   },
   {
     name: "Greg Jones",
-    profileId: "567",
+    profileId: "5667",
     profileImageUrl: "https://i.pravatar.cc/120?img=6",
   },
 ];
