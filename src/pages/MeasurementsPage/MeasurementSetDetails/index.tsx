49e35cc42bf02d71b2628b797e83232373b9856b
Lukas <lpetersonrhs@gmail.com>
2022-01-09 11:22:50 -0600

    Starting new nested routes

---
+++ src/pages/MeasurementsPage/MeasurementSetDetails/index.tsx
@@ -0,0 +1,9 @@
+import { VFC } from "react";
+import { Route, useHistory } from "react-router-dom";
+import { MeasurementSet } from "../../../core/configurator-types";
+
+const MeasurementSetDetails: VFC<MeasurementSet> = (measurementSet) => {
+  return <div>Measurement Set Details Works!</div>;
+};
+
+export default MeasurementSetDetails;
