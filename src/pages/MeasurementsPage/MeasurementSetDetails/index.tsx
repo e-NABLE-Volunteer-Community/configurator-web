7ef808f0c96c52ff8a065e78c407d40ba9cf4a40
Lukas <lpetersonrhs@gmail.com>
2022-01-15 09:42:47 -0600

    Get measurement set sent to MeasurementSetDetails component

--- src/pages/MeasurementsPage/MeasurementSetDetails/index.tsx
+++ src/pages/MeasurementsPage/MeasurementSetDetails/index.tsx
@@ -1,9 +1,17 @@
 import { VFC } from "react";
 import { Route, useHistory } from "react-router-dom";
 import { MeasurementSet } from "../../../core/configurator-types";
+import { useProfileMeasurementSet } from "../../../core/hooks/measurements";
+import { isLoading, Loading } from "../../../core/stores/utils";
 
-const MeasurementSetDetails: VFC<MeasurementSet> = (measurementSet) => {
-  return <div>Measurement Set Details Works!</div>;
+const MeasurementSetDetails: VFC<MeasurementSet> = () => {
+  const profileMeasurementSet = useProfileMeasurementSet();
+
+  if (!profileMeasurementSet || isLoading(profileMeasurementSet))
+    return <div>loading</div>;
+
+  console.log("m: ", profileMeasurementSet);
+  return <div>{profileMeasurementSet.name}</div>;
 };
 
 export default MeasurementSetDetails;
