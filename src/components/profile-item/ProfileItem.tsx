3066966bc87b870a1faebc683ba69ab9ca064383
Daemongear <->
2022-01-29 00:39:51 +0200

    modified profile-item page

--- src/components/profile-item/ProfileItem.tsx
+++ src/components/profile-item/ProfileItem.tsx
@@ -1,8 +1,9 @@
-import { IonItem, IonImg } from "@ionic/react";
+import { IonItem, IonImg, IonLabel, IonIcon } from "@ionic/react";
 import { MouseEventHandler, VFC } from "react";
 import { useHistory } from "react-router";
 import { Profile } from "../../core/profile-types";
 import "../profile-item/profile-item.scss";
+import { chevronForwardOutline, link } from "ionicons/icons";
 
 export type ProfileItem = {
   profile: Profile;
@@ -12,19 +13,21 @@ export type ProfileItem = {
 const ProfileItem: VFC<ProfileItem> = ({ profile, onProfileClick }) => {
   const key = profile.profileId;
   return (
-    <IonItem
-      key={key}
-      onClick={onProfileClick}
-      className="profile-list-item-base"
-    >
-      <div className="profile-list-item">
-        <IonImg className="profile-image" src={profile.profileImageUrl} />
-        <div className="profile-info">
-          <h5>{profile.name}</h5>
-          <div className="profile-measurement-count">3 measurements</div>
-          <div className="profile-location">location</div>
+    <IonItem key={key} onClick={onProfileClick}>
+      <IonLabel>
+        <div className="profile-item-label">
+          <IonImg
+            className="profile-item-image"
+            src={profile.profileImageUrl}
+          />
+          <div className="profile-item-display">
+            <div className="profile-item-name">{profile.name}</div>
+            <div>3 measurements</div>
+            <div className="profile-item-location">location</div>
+          </div>
         </div>
-      </div>
+      </IonLabel>
+      <IonIcon className="profile-arrow" icon={chevronForwardOutline} />
     </IonItem>
   );
 };
