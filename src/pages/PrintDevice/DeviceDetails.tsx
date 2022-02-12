1a61019793ebad5153213c6c1cf602ce183d10f4
Lukas <lpetersonrhs@gmail.com>
2022-02-12 11:11:04 -0600

    Set up route matching for device details

--- src/pages/PrintDevice/DeviceDetails.tsx
+++ src/pages/PrintDevice/DeviceDetails.tsx
@@ -4,12 +4,15 @@ import { useHistory } from "react-router";
 import { isLoading } from "../../core/stores/utils";
 import "../PrintDevice/print-device.scss";
 import DeviceDetails from "../../components/device-details/DeviceDetails";
-import { useActiveDevice } from "../../core/hooks/devices";
+import {
+  useActiveDevice,
+  useActivePrintDevice,
+} from "../../core/hooks/devices";
 import { Device } from "../../core/onshape-types";
 import HeaderSmall from "../../components/header/header-small";
 
 const PrintDeviceDeviceDetails: VFC = () => {
-  const device = useActiveDevice();
+  const device = useActivePrintDevice();
   const history = useHistory();
   const onDeviceDetailsClick = (device: Device) => history.push("123123");
   if (isLoading(device) || !device) return <div>loading...</div>;
